ce13747265149a5777702bac9286caa600083dc8
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-22 18:56:00 +0100

    WIP revert disabled USE_*=OFF

--- T/Torch/build_tarballs.jl
+++ T/Torch/build_tarballs.jl
@@ -105,9 +105,9 @@ fi
 #     cmake_extra_args+=(-DUSE_PYTORCH_QNNPACK=OFF)
 # fi
 
-if [[ $target == aarch64-linux-gnu* # Disabled use of breakpad on aarch64-linux-gnu: Fails to build embedded breakpad library.
-#     || $target == *-w64-mingw32* # Disabling breakpad enables configure on Windows - in combination with pytorch-aten-qnnpack-cmake-windows.patch
-#     || $target == *-freebsd*
+if [[ $target == aarch64-linux-gnu* # Fails to build embedded breakpad library.
+    || $target == *-w64-mingw32* # Disabling breakpad enables configure on Windows - in combination with pytorch-aten-qnnpack-cmake-windows.patch
+    || $target == *-freebsd* # Fails to build embedded breakpad library
 ]]; then
     cmake_extra_args+=(-DUSE_BREAKPAD=OFF)
 else
